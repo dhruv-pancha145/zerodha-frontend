e74fd802a1763cd074865b4fa1c9f4dff9a99124
Dhruv Panchal <dhruvpanchal1345@gmail.com>
2026-06-16 17:30:27 +0530

    update dashboard URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         navigate("/login");
       }
       const { data } = await axios.post(
-        "http://localhost:3000",
+        "zerodha-frontend-lovat.vercel.app",
         {},
         { withCredentials: true }
       );
